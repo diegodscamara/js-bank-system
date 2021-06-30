4343d3e8ba48f405b0bcbffeb38e050057b8c645
Your Namegit <you@example.com>
2021-06-30 16:03:27 -0300

    json

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,11 @@
 export class ContaCorrente {
     agencia;
+    cliente;
+
+
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -15,5 +19,12 @@ export class ContaCorrente {
             return;
         }
         this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
     }
 }
